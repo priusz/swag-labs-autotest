3534a9c1b85e765a3f2f556595ccc32909290b48
priusz <varro.timi@gmail.com>
2024-06-27 14:52:51 +0200

    implement LoginStep methods

--- src/main/java/com/codecool/LoginPage.java
+++ src/main/java/com/codecool/LoginPage.java
@@ -5,28 +5,43 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends BasePage{
+
     public LoginPage(WebDriver driver) {
         super(driver);
     }
+
     @FindBy(id = "user-name")
     private WebElement usernameField;
+
     @FindBy(id = "password")
     private WebElement passwordField;
+
     @FindBy(id = "login-button")
     private WebElement loginButton;
+
+    @FindBy(css = "[data-test='title']")
+    private WebElement productTextField;
+
 
     private void fillUsernameField(String username) {
         usernameField.sendKeys(username);
     }
+
     private void fillPasswordField(String password) {
         passwordField.sendKeys(password);
     }
+
     private void clickLoginButton() {
         loginButton.click();
     }
+
     public void handleLogin(String username, String password) {
         fillUsernameField(username);
         fillPasswordField(password);
         clickLoginButton();
+    }
+
+    public String getProductTextField() {
+        return productTextField.getText();
     }
 }
